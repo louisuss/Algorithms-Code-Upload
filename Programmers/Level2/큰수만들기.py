9b1db59af583539a6ef11602475dca4f6e940352
louis <dobi1115@gmail.com>
2020-06-22 21:56:42 +0900

    Solution for greedy problem

---
+++ Programmers/Level2/큰수만들기.py
@@ -0,0 +1,16 @@
+from itertools import combinations
+
+
+# def solution(number, k):
+#     answer = []
+#     number = list(number)
+#     answer2 = []
+#     for i in combinations(number, len(number) - k):
+#         answer.append(i)
+#     for i in answer:
+#         answer2.append(int(''.join(i)))
+
+#     return str(max(answer2))
+a = [1, 2, 3, 4]
+
+print(a.index(1))
